7276301302d57ebbb517ea81278c994bce378606
Harsh Singh <harsh220902@gmail.com>
2022-03-26 21:23:52 +0530

    activity page web view

--- app/src/main/java/com/tutorial/aad_project/activity.java
+++ app/src/main/java/com/tutorial/aad_project/activity.java
@@ -4,19 +4,56 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class activity extends Fragment {
     public activity() {
         // Required empty public constructor
     }
 
+    public WebView webView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity, container, false);
+
+        super.onCreate(savedInstanceState);
+
+        View v = inflater.inflate(R.layout.fragment_activity, container, false);
+        webView = v.findViewById(R.id.webView);
+        webView.loadUrl("https://www.businessinsider.in/?r=US&IR=T");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    WebView webView = (WebView) v;
+
+                    switch(keyCode) {
+                        case KeyEvent.KEYCODE_BACK:
+                            if (webView.canGoBack()) {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
+        
+        return v;
     }
+
 }
